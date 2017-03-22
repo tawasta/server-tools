a8370f78937fcc5a98eab137b22bf0490ba3626a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-03-22 10:56:35 +0200

    Add functionality for using a company-spesific sender mail

--- mail_server_multicompany/models/ir_mail_server.py
+++ mail_server_multicompany/models/ir_mail_server.py
@@ -40,20 +40,53 @@ class IrMailserver(models.Model):
                    smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False):
 
         if not mail_server_id:
-            company = self.create_uid.company_id
+            # Use mail message id meta information for getting the sending company
+            # TODO: could this be done with more reliable way?
+            references = message['references']
 
-            print self
-            print message
-            print self.create_uid
-            print self.write_uid
-            print self._uid
+            # Remove the hostname part
+            try:
+                references = references.split("@")[0]
+            except Exception, e:
+                _logger.warning(e)
+
+            try:
+                # Split the parts to a list. The list should look like this:
+                # ['<1490169823.917293071746826.735164174278517', 'openerp', '1234', 'crm.claim']
+                # [{id-numbers that we don't use here}, {static 'openerp'}, {model instance id}, {model}]
+                references_list = references.split("-")
+
+                model_instance_id = references_list[2]
+                model_name = references_list[3]
+
+            except Exception, e:
+                _logger.warning(e)
+
+                model_instance_id = False
+                model_name = False
+
+            if model_name and model_instance_id:
+                # Try to find the model instance
+                try:
+                    model = self.env[model_name]
+                    model_instance = model.browse([int(model_instance_id)])
+
+                    if hasattr(model_instance, 'company_id'):
+                        company_id = company = getattr(model_instance, 'company_id').id
+                    elif hasattr(model_instance, 'company'):
+                        company_id = getattr(model_instance, 'company').id
+                    else:
+                        company_id = False
+
+                except Exception, e:
+                    _logger.warning(e)
+                    company_id = False
 
             mail_server = self.env['ir.mail_server'].search([
-                ('company', '=', company.id)
-            ], limit=1, order='priority DESC')
+                ('company', '=', company_id)
+            ], limit=1, order='sequence DESC')
 
             if mail_server:
-                print mail_server.name
                 mail_server_id = mail_server.id
 
         return super(IrMailserver, self).send_email(
