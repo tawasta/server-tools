b7f1c651bd7711af619dd6a1aa85d51f91757301
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-05-19 20:15:53 +0300

    Improve formatting and readme

---
+++ auth_ldap_only/README.rst
@@ -0,0 +1,51 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==============
+Auth LDAP Only
+==============
+
+Authentication via LDAP Only
+Disables logins against local passwords in Odoo res_users database.
+
+Features
+--------
+
+* Only allows LDAP-authorization (disables local logins)
+* Admin is excluded from LDAP-authorization, and can login via local password
+* Users can be marked to bypass ldap (API users etc.)
+
+Installation
+============
+
+Install the module form Settings->Local Modules
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
--- auth_ldap_only/__openerp__.py
+++ auth_ldap_only/__openerp__.py
@@ -1,42 +1,44 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2017 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
+
 {
-    'name': 'Authentication via LDAP Only',
-    'category': 'Authentication',
-    'version': '1.0',
+    'name': 'Auth LDAP only',
+    'summary': 'Authentication via LDAP Only',
+    'version': '8.0.1.1.0',
+    'category': 'Tools',
+    'website': 'https://github.com/Tawasta/server-tools',
     'author': 'Oy Tawasta Technologies Ltd',
-    'website': 'http://www.tawasta.fi',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
     'depends': [
         'auth_ldap',
     ],
     'data': [
-        'view/res_users_form.xml',
+        'views/res_users_form.xml',
     ],
-    'description': 
-'''
-Auth LDAP Only
---------------
-
-Auth LDAP Only is an extension to [auth\_ldap](https://github.com/OCA/OCB/tree/8.0/addons/auth_ldap)
-
-Disables logins against local passwords in Odoo res_users database.
-
-Features
---------
-
-* Only allows LDAP-authorization (disables local logins)
-* Admin is excluded from LDAP-authorization, and can login via local password
-'''
+    'demo': [
+    ],
 }
