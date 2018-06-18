b35af7a514122e524737e322d4cfe010aa6189f4
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-18 16:07:22 +0300

    Add a a new module for refreshing dashboard automatically

---
+++ board_auto_refresh/static/src/js/auto_refresh.js
@@ -0,0 +1,29 @@
+odoo.define('web_auto_refresh.board', function (require) {
+    "use strict";
+
+    var refresh_page = function(view) {
+        setInterval(function() {
+            var active_view = view.action_manager.inner_widget.active_view;
+            if (typeof(active_view) != 'undefined'){
+                try {
+                    var controller = view.action_manager.inner_widget.active_view.controller;
+                    var action = view.action_manager.inner_widget.action;
+
+                    if (String(action.view_id).match('My Dashboard')) {
+                        controller.reload();
+                    }
+                } catch (e) {
+                    alert(e);
+                }
+            }
+        }, 300000);
+    }
+
+    openerp.web.WebClient.include({
+        start: function() {
+            this._super();
+            refresh_page(this);
+        },
+
+    });
+});
