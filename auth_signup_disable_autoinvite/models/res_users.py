b64d764446a8186d65d4701ec6868a53dbfa3538
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-11-08 15:23:03 +0200

    Add new module for disabling signup auto invite

---
+++ auth_signup_disable_autoinvite/models/res_users.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.model
+    def create(self, vals):
+        return super(ResUsers, self.with_context(
+            no_reset_password=True)).create(vals)
