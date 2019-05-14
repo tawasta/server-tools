13dab64a9aeeedae77ce7960148837ab912fef74
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-05-14 14:57:09 +0300

    [REM] Obsolete permission

--- base_read_access_administration/__manifest__.py
+++ base_read_access_administration/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Read Access Group for Administration',
     'summary': 'Adds a new group that can read administration data',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Extra Rights',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
