193703fd7798e7ff8606650869d2e1624af1a6b7
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-12-30 14:29:27 +0200

    [IMP] More secure provider filtering logic

--- auth_oauth_hidden/controllers/auth_oauth_login.py
+++ auth_oauth_hidden/controllers/auth_oauth_login.py
@@ -11,20 +11,27 @@ class OAuthLoginHidden(OAuthLogin):
         # Remove hidden providers, unless a GET parameter is given
         if res.qcontext.get('providers'):
             key = kw.get('key', False)
+            res.qcontext['providers'] = []
+
             for provider in res.qcontext['providers']:
                 hidden = provider.get('hidden', False)
 
                 if hidden and key != provider.get('hidden_key'):
-                    res.qcontext['providers'].remove(provider)
+                    continue
+
+                res.qcontext['providers'].append(provider)
 
         return res
 
     def get_auth_signup_qcontext(self):
         res = super(OAuthLoginHidden, self).get_auth_signup_qcontext()
+        res['providers'] = []
 
         # Don't show hidden providers in signup
         for provider in res.get('providers'):
             if provider.get('hidden'):
-                res['providers'].remove(provider)
+                continue
+
+            res['providers'].append(provider)
 
         return res
