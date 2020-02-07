cace004e16846fa5030d33f89c785f1b3ff8df87
retropikzel <retropikzel@mailbox.org>
2020-02-07 10:15:36 +0200

    [FIX] Boot autoupgrade endless loop prevention

--- boot_autoupgrade_helper/models/init.py
+++ boot_autoupgrade_helper/models/init.py
@@ -1,12 +1,18 @@
 from odoo import models, api, fields
 import datetime
 
+class LastBootUpgradeTimeCompany(models.Model):
+    _inherit = 'res.company'
+    last_boot_upgrade = fields.Datetime(
+        string='Last time all module upgrade happened on boot',
+        default=datetime.datetime.now() - datetime.timedelta(hours=1),
+        readonly=False)
+
 class Module(models.Model):
     _inherit = 'ir.module.module'
-    last_boot_upgrade = fields.Datetime.context_timestamp(self, timestamp=datetime.datetime.now())
 
     @api.model
     def boot_upgrade(self):
-        if last_boot_update < datetime.datetime.now() - datetime.timedelta(seconds=10):
-            last_boot_upgrade = datetime.datetime.now()
+        if self.env.user.company_id.last_boot_upgrade < datetime.datetime.now() - datetime.timedelta(seconds=60):
+            self.env.user.company_id.last_boot_upgrade = datetime.datetime.now()
             return super(Module, self).upgrade_changed_checksum(self)
