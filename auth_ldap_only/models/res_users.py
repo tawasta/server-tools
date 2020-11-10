bae5c209a3c92b763c71a10ebe7acb8edca369f1
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-11-10 10:31:30 +0200

    [FIX] LDAP login bypass

--- auth_ldap_only/models/res_users.py
+++ auth_ldap_only/models/res_users.py
@@ -1,18 +1,30 @@
-from odoo import api, models
+from odoo import models
 from odoo import SUPERUSER_ID
 from odoo.exceptions import AccessDenied
 
 
-class ResUsers(models.Model):
+class Users(models.Model):
     _inherit = "res.users"
 
-    def check_credentials(self, password):
-        if self._uid == SUPERUSER_ID or self.has_group("auth_ldap_only.ldap_bypass"):
-            """
-            Use normal login process for
-            - Admin to prevent locking everyone out
-            - Users that belongs to "Bypass LDAP"-group
-            """
-            return super(ResUsers, self).check_credentials(password=password)
-        else:
-            raise AccessDenied()
+    def _check_credentials(self, password):
+        try:
+            if self._uid == SUPERUSER_ID or self.has_group(
+                "auth_ldap_only.ldap_bypass"
+            ):
+                """
+                Use normal login process for
+                - Admin to prevent locking everyone out
+                - Users that belongs to "Bypass LDAP"-group
+                """
+                super(Users, self)._check_credentials(password)
+            else:
+                # Raise AccessDenied to try LDAP login
+                raise AccessDenied
+        except AccessDenied:
+            # LDAP Login
+            if self.env.user.active:
+                Ldap = self.env["res.company.ldap"]
+                for conf in Ldap._get_ldap_dicts():
+                    if Ldap._authenticate(conf, self.env.user.login, password):
+                        return
+            raise
