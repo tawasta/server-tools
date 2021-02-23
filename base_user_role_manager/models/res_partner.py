2e1efe32911cadf221b6795869580273e4e999a0
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-02-23 16:55:25 +0200

    [ADD] New module base_user_role_manager
    Allows user to manage users and roles

---
+++ base_user_role_manager/models/res_partner.py
@@ -0,0 +1,16 @@
+from odoo import fields
+from odoo import models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    signup_token = fields.Char(
+        groups="base.group_erp_manager,base_user_role_manager.group_role_manager"
+    )
+    signup_type = fields.Char(
+        groups="base.group_erp_manager,base_user_role_manager.group_role_manager"
+    )
+    signup_expiration = fields.Datetime(
+        groups="base.group_erp_manager,base_user_role_manager.group_role_manager"
+    )
