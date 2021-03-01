bc8c8b7714eded483f055fc3c1ebc5c7925874b3
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-03-01 15:43:09 +0200

    [ADD] New module company_display_name_recursive
    Show recursive company name

---
+++ company_display_name_recursive/__manifest__.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2021 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Company recursive display name",
+    "summary": "Show parent(s) in company name",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/Tawasta/server-tools",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+}
