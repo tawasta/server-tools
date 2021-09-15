afa576f6e03a970c8c45275d5de64448f998d7e0
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-09-15 13:57:14 +0300

    Modified function

--- delete_inactive_users/models/res_users.py
+++ delete_inactive_users/models/res_users.py
@@ -20,3 +20,23 @@ class ResUsers(models.Model):
                 days_in_system = date_now.date() - user.create_date.date()
                 if int(days_in_system.days) > int(inactive_users_val):
                     user.sudo().write({"active": False})
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        login_key = "login"
+        lastname_key = "lastname"
+        firstname_key = "firstname"
+        login_of_key = [a_dict[login_key] for a_dict in vals_list]
+        lastname_of_key = [a_dict[lastname_key] for a_dict in vals_list]
+        firstname_of_key = [a_dict[firstname_key] for a_dict in vals_list]
+        already_user_by_email = self.env["res.users"].sudo().search([
+            ('login', '=ilike', login_of_key[0]),
+            ('lastname', '=ilike', lastname_of_key[0]),
+            ('firstname', '=ilike', firstname_of_key[0]),
+            ('active', '=', False),
+        ])
+        if already_user_by_email and already_user_by_email.active == False:
+            already_user_by_email.sudo().write({"active": True})
+            return already_user_by_email
+        else:
+            return super(ResUsers, self).create(vals_list)
