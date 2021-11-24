33386a4e08a0e00223b94479d8f2038abad17061
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-11-24 13:28:56 +0200

    Created a new module

---
+++ auth_signup_partner_ref_login/controllers/main.py
@@ -0,0 +1,24 @@
+from odoo import http, _
+from odoo.http import request
+from odoo.exceptions import UserError
+
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome
+
+
+class AuthSignupHome(AuthSignupHome):
+
+     def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = { key: qcontext.get(key) for key in ('login', 'name', 'email', 'password') }
+        if not values:
+            raise UserError(_("The form was not properly filled in."))
+        if values.get('password') != qcontext.get('confirm_password'):
+            raise UserError(_("Passwords do not match; please retype them."))
+        supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
+        lang = request.context.get('lang', '')
+        ref = qcontext.get('login')
+        values['ref'] = ref
+        if lang in supported_lang_codes:
+            values['lang'] = lang
+        self._signup_with_values(qcontext.get('token'), values)
+        request.env.cr.commit()
