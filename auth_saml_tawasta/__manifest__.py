bfadcb200f5e2c0c37f467a8019833b7aa9218d6
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2022-08-24 07:45:22 +0000

    14.0 auth saml tawasta

---
+++ auth_saml_tawasta/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2020 GlodoUK <https://www.glodo.uk/>
+# Copyright (C) 2010-2016, 2022 XCG Consulting <http://odoo.consulting>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SAML2 Authentication Tawasta",
+    "version": "14.0.1.1.0",
+    "category": "Tools",
+    "author": "XCG Consulting, Odoo Community Association (OCA), Tawasta",
+    "website": "https://gitlab.com/tawasta/odoo/server-tools",
+    "license": "AGPL-3",
+    "depends": ["base_setup"],
+    "external_dependencies": {
+        "python": ["pysaml2"],
+        "bin": ["xmlsec1"],
+        # special definition used by OCA to install packages
+        "deb": ["xmlsec1"],
+    },
+    "demo": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/auth_saml.xml",
+        "views/res_config_settings.xml",
+        "views/res_users.xml",
+        "data/ir_config_parameter.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "development_status": "Beta",
+}
---
+++ auth_saml_tawasta/models/__init__.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    auth_saml_attribute_mapping,
+    auth_saml_provider,
+    auth_saml_request,
+    ir_config_parameter,
+    res_config_settings,
+    res_users,
+    res_users_saml,
+    auth_saml_attribute_entity,
+)
