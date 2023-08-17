a235328f51e971502ddd42aada1387c50110338d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-08-17 15:40:21 +0300

    [MIG] 16.0 auth_oauth_gsuite

---
+++ auth_oauth_gsuite/controllers/oauth_login.py
@@ -0,0 +1,21 @@
+from werkzeug.urls import url_encode
+
+from odoo.addons.auth_oauth.controllers.main import OAuthLogin
+
+
+class OAuthLoginGsuite(OAuthLogin):
+    def list_providers(self):
+        res = super(OAuthLoginGsuite, self).list_providers()
+
+        for provider in res:
+            if provider.get("hd"):
+                params = dict(
+                    hd=provider.get("hd"),
+                )
+
+                provider["auth_link"] = "%s&%s" % (
+                    provider["auth_link"],
+                    url_encode(params),
+                )
+
+        return res
