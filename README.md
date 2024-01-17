4b738508f77016c6ba3c0ec898ce6a0f7c64f413
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2024-01-17 13:39:09 +0200

    [ADD] mattermost_hook and auth_oauth_portal_register ports

--- README.md
+++ README.md
@@ -12,10 +12,12 @@ addon | version | maintainers | summary
 --- | --- | --- | ---
 [auth_oauth_gsuite](auth_oauth_gsuite/) | 17.0.1.0.0 |  | Adds Google G Suite specific options
 [auth_oauth_hidden](auth_oauth_hidden/) | 17.0.1.0.0 |  | Adds a parameter for OAuth providers to allow hiding them
+[auth_oauth_portal_register](auth_oauth_portal_register/) | 17.0.1.0.0 |  | Enables registering with OAuth without opening signup
 [auth_signup_disable_autoinvite](auth_signup_disable_autoinvite/) | 17.0.1.0 |  | Disable signup auto invite
 [auth_signup_invite_mail_system_name](auth_signup_invite_mail_system_name/) | 17.0.1.0 |  | Adds field to res.company to define system name. Changes invite mail.
 [auth_signup_mass_action_invite](auth_signup_mass_action_invite/) | 17.0.1.0 |  | Send invite mail with mass action
 [base_user_role_manager](base_user_role_manager/) | 17.0.1.1 |  | Allow user to manage users and their roles
 [ir_ui_view_search](ir_ui_view_search/) | 17.0.1.0.0 |  | Ability to search ir_ui_view by external id and module
+[mattermost_hook](mattermost_hook/) | 17.0.1.0.0 |  | Adds possible to use Mattermost hooks
 
 [//]: # (end addons)
---
+++ mattermost_hook/models/mattermost_hook.py
@@ -0,0 +1,106 @@
+import requests
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+
+class MattermostHook(models.Model):
+    _name = "mattermost.hook"
+    _description = "Mattermost hooks"
+
+    name = fields.Char(
+        required=True,
+    )
+    mattermost_url = fields.Char(
+        string="Mattermost URL",
+        help="Mattermost URL (without trailing backlash)",
+        required=True,
+    )
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        help="Company who owns the hook",
+        default=lambda self: self.env.company,
+        required=True,
+    )
+    res_model = fields.Many2one(
+        "ir.model",
+        string="Model",
+        help="Which model this hook is related to",
+        required=True,
+        ondelete="cascade",
+    )
+    function = fields.Char(
+        string="Method",
+        help="Name of the method to be called",
+        required=True,
+    )
+    hook = fields.Char(
+        string="Hook key",
+        help="The hook generated key",
+    )
+    channel = fields.Char(
+        help="Channel the message is posted to",
+    )
+    username = fields.Char(
+        help="Username who posts the message to Mattermost",
+    )
+    icon_url = fields.Char(
+        string="Icon URL",
+        help="Icon url to be used for posting the message",
+    )
+
+    def _get_hook_url(self):
+        return "{}/hooks/{}".format(self.mattermost_url, self.hook)
+
+    def post_mattermost(
+        self, message, channel=False, username=False, icon_url=False, verify=True
+    ):
+        """
+        Method to post a message to the hook
+
+        :param message: translated message
+        :param channel: channel name
+        :param username: username to be posted as
+        :param icon_url: URL of an icon to be used
+        :param verify: Boolean, is the certificate verified
+        :return: None
+        :raise ValidationError: Raises exception if no message
+        """
+        self.ensure_one()
+        hook_url = self._get_hook_url()
+
+        if not message.strip():
+            raise ValidationError(_("Message can't be empty!"))
+        payload = {
+            "text": message,
+        }
+
+        if not channel:
+            channel = self.channel
+
+        if not username:
+            username = self.username
+
+        if not icon_url:
+            icon_url = self.icon_url
+
+        if channel:
+            payload["channel"] = channel
+        if username:
+            payload["username"] = username
+        if icon_url:
+            payload["icon_url"] = icon_url
+        res = requests.post(hook_url, json=payload, verify=verify, timeout=10)
+        if res.status_code != 200:
+            raise requests.exceptions.HTTPError(res.text)
+
+    def action_test_hook(self):
+        """Test if hook is working"""
+        self.ensure_one()
+        try:
+            test_msg = "It worked, great job. Go nuts!"
+            self.post_mattermost(test_msg)
+        except Exception:
+            msg = "An error occured, most likely cause hook settings are incorrect!"
+            raise UserError(msg) from Exception
