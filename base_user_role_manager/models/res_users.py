b51838631a66aa1921ac98eb48d7bf45a1b5697e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-09-23 09:44:34 +0300

    [FIX] Allow user managers to handle roles

--- base_user_role_manager/models/res_users.py
+++ base_user_role_manager/models/res_users.py
@@ -4,6 +4,10 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
+    role_ids = fields.One2many(
+        groups="base.group_erp_manager,base_user_role_manager.group_role_manager",
+    )
+
     role_line_ids = fields.One2many(
         groups="base.group_erp_manager,base_user_role_manager.group_role_manager",
     )
