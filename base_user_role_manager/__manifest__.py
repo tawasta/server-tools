00b1800170daf031605d15cefaa988d4135efb29
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-05-28 13:30:19 +0300

    [IMP] Restrict editing admin users

--- base_user_role_manager/__manifest__.py
+++ base_user_role_manager/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "An user group for managing users and roles",
     "summary": "Allow user to manage users and their roles",
-    "version": "17.0.1.1.3",
+    "version": "17.0.1.1.4",
     "category": "Tools",
     "website": "https://github.com/tawasta/server-tools",
     "author": "Futural",
@@ -33,7 +33,8 @@
     "data": [
         "security/base_groups.xml",
         "security/model_access.xml",
-        "views/menu.xml",
+        "views/menu_action.xml",
+        "views/menu_item.xml",
         "views/res_users.xml",
     ],
     "demo": [],
