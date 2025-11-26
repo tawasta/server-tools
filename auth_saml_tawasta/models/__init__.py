cae5fd692c48701ce62e2cccc0f432fd0e4c8b37
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-26 14:24:34 +0200

    Backup

--- auth_saml_tawasta/models/__init__.py
+++ auth_saml_tawasta/models/__init__.py
@@ -1,5 +1,5 @@
 from . import auth_saml_attribute_entity
 from . import auth_saml_provider
 from . import auth_saml_token
-from . import ir_config_parameter
+#from . import ir_config_parameter
 #from . import res_users
