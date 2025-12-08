a1f432a3a6bd0c16926b2e27f75147949a3c968b
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-12-08 13:28:42 +0200

    Make new branch for clean merge into 17.0

---
+++ auth_saml_tawasta/controllers/main.py
@@ -0,0 +1,344 @@
+# Copyright (C) 2020 GlodoUK <https://www.glodo.uk/>
+# Copyright (C) 2010-2016 XCG Consulting <http://odoo.consulting>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import functools
+import json
+import logging
+import re
+import urllib
+
+# dependency name is pysaml2 # pylint: disable=W7936
+import saml2.xmldsig as ds
+import werkzeug.utils
+from saml2 import BINDING_HTTP_REDIRECT
+from saml2.ident import code, decode
+from saml2.response import StatusError
+from saml2.time_util import in_a_while
+from werkzeug.urls import url_quote_plus, url_unquote_plus
+
+import odoo
+from odoo import SUPERUSER_ID, _, api, exceptions, http, registry as registry_get
+from odoo.http import request
+from odoo.tools.misc import clean_context
+
+from odoo.addons.web.controllers.utils import _get_login_redirect_url, ensure_db
+from odoo.addons.web.controllers.session import Session
+from odoo.addons.web.controllers.home import Home
+from odoo.addons.auth_saml.controllers.main import AuthSAMLController
+
+_logger = logging.getLogger(__name__)
+
+
+# ----------------------------------------------------------
+# helpers
+# ----------------------------------------------------------
+
+
+def fragment_to_query_string(func):
+    @functools.wraps(func)
+    def wrapper(self, **kw):
+        if not kw:
+            return """<html><head><script>
+                var l = window.location;
+                var q = l.hash.substring(1);
+                var r = '/' + l.search;
+                if(q.length !== 0) {
+                    var s = l.search ? (l.search === '?' ? '' : '&') : '?';
+                    r = l.pathname + l.search + s + q;
+                }
+                window.location = r;
+            </script></head><body></body></html>"""
+        return func(self, **kw)
+
+    return wrapper
+
+
+# ----------------------------------------------------------
+# Controller
+# ----------------------------------------------------------
+
+
+class SAMLLogin(Home):
+    """
+    def _list_saml_providers_domain(self):
+        return []
+
+    def list_saml_providers(self, with_autoredirect: bool = False):
+        providers = (
+            request.env["auth.saml.provider"]
+            .sudo()
+            .search_read(self._list_saml_providers_domain())
+        )
+
+        for provider in providers:
+            # Compatibility with auth_oauth/controllers/main.py in order to
+            # avoid KeyError rendering template_auth_oauth_providers
+            provider.setdefault("auth_link", "")
+        return providers
+    """
+
+    @http.route()
+    def web_login(self, *args, **kw):
+        ensure_db()
+        if (
+            request.httprequest.method == "GET"
+            and request.session.uid
+            and request.params.get("redirect")
+        ):
+
+            # Redirect if already logged in and redirect param is present
+            return http.redirect_with_hash(request.params.get("redirect"))
+
+        providers = self.list_saml_providers()
+
+        response = super().web_login(*args, **kw)
+        if response.is_qweb:
+            error = request.params.get("saml_error")
+            if error == "no-signup":
+                error = _("Sign up is not allowed on this database.")
+            elif error == "access-denied":
+                error = _("Access Denied")
+            elif error == "expired":
+                error = _(
+                    "You do not have access to this database. Please contact"
+                    " support."
+                )
+            else:
+                error = None
+
+            # Tawasta: Distinct SAML from OAuth
+            response.qcontext["saml_providers"] = providers
+
+            if error:
+                response.qcontext["error"] = error
+
+        return response
+
+
+class TawastaAuthSAMLController(AuthSAMLController):
+    def _get_saml_extra_relaystate(self):
+        """
+        Compute any additional extra state to be sent to the IDP so it can
+        forward it back to us. This is called RelayState.
+
+        The provider will automatically set things like the dbname, provider
+        id, etc.
+        """
+
+        redirect = request.params.get("redirect") or "/"
+        if not redirect.startswith(("//", "http://", "https://")):
+            redirect = "{}{}".format(
+                request.httprequest.url_root,
+                redirect[1:] if redirect[0] == "/" else redirect,
+            )
+
+        state = {
+            "r": url_quote_plus(redirect),
+        }
+        return state
+
+    @http.route("/auth_saml/get_auth_request", type="http", auth="none")
+    def get_auth_request(self, pid, redirect=None):
+        provider_id = int(pid)
+
+        provider = request.env["auth.saml.provider"].sudo().browse(provider_id)
+        redirect_url = provider._get_auth_request(
+            self._get_saml_extra_relaystate(), request.httprequest.url_root.rstrip("/")
+        )
+        if not redirect_url:
+            raise Exception(
+                "Failed to get auth request from provider. "
+                "Either misconfigured SAML provider or unknown provider."
+            )
+
+        redirect = werkzeug.utils.redirect(redirect_url, 303)
+        redirect.autocorrect_location_header = True
+        return redirect
+
+    @http.route("/auth_saml/signin", type="http", auth="none", csrf=False)
+    @fragment_to_query_string
+    def signin(self, **kw):
+        """
+        Client obtained a saml token and passed it back
+        to us... we need to validate it
+        """
+        saml_response = kw.get("SAMLResponse")
+
+        if not kw.get("RelayState"):
+            # here we are in front of a client that went through
+            # some routes that "lost" its relaystate... this can happen
+            # if the client visited his IDP and successfully logged in
+            # then the IDP gave him a portal with his available applications
+            # but the provided link does not include the necessary relaystate
+            url = "/?type=signup"
+            redirect = werkzeug.utils.redirect(url, 303)
+            redirect.autocorrect_location_header = True
+            return redirect
+
+        state = json.loads(kw["RelayState"])
+        provider = state["p"]
+        dbname = state["d"]
+        if not http.db_filter([dbname]):
+            return BadRequest()
+        ensure_db(db=dbname)
+
+        request.update_context(**clean_context(state.get("c", {})))
+        try:
+            credentials = (
+                request.env["res.users"]
+                .with_user(SUPERUSER_ID)
+                .auth_saml(
+                    provider,
+                    saml_response,
+                    request.httprequest.url_root.rstrip("/"),
+                )
+            )
+            action = state.get("a")
+            menu = state.get("m")
+            redirect = (
+                werkzeug.urls.url_unquote_plus(state["r"]) if state.get("r") else False
+            )
+            url = "/web"
+            if redirect:
+                url = redirect
+            elif action:
+                url = "/#action=%s" % action
+            elif menu:
+                url = "/#menu_id=%s" % menu
+            request.session["_saml_user"] = True
+            pre_uid = request.session.authenticate(*credentials)
+            resp = request.redirect(_get_login_redirect_url(pre_uid, url), 303)
+            resp.autocorrect_location_header = False
+            return resp
+
+        except exceptions.AccessDenied:
+            # saml credentials not valid,
+            # user could be on a temporary session
+            _logger.info("SAML2: access denied")
+
+            url = "/web/login?saml_error=expired"
+            redirect = werkzeug.utils.redirect(url, 303)
+            redirect.autocorrect_location_header = False
+            return redirect
+
+        except Exception as e:
+            # signup error
+            _logger.exception("SAML2: failure - %s", str(e))
+            url = "/web/login?saml_error=access-denied"
+
+        redirect = request.redirect(url, 303)
+        redirect.autocorrect_location_header = False
+        return redirect
+
+    @http.route(["/<string:provider_name>/auth_saml/metadata", "/auth_saml/metadata"], type="http", auth="none", csrf=False)
+    def saml_metadata(self, **kw):
+        provider_name = kw.pop('provider_name', False)
+        if provider_name:
+            provider = request.env['auth.saml.provider'].with_user(SUPERUSER_ID).search([
+                ('name', '=', provider_name)
+            ])
+            if len(provider) == 0:
+                return request.make_response(
+                    "No such provider",
+                    headers=[],
+                    status=500,
+                )
+            else:
+                if provider[0].sp_baseurl:
+                    base_url = provider[0].sp_baseurl
+                else:
+                    base_url = None
+                return request.make_response(
+                    provider[0]._metadata_string(base_url=base_url),
+                    headers=[('Content-Type', 'application/xml')],
+                    status=200
+                )
+        else:
+            super.saml_metadata(**kw)
+
+    @http.route("/auth_saml/slo", type="http", auth="none", csrf=False)
+    @fragment_to_query_string
+    def slo_request(self, req, *args, **kwargs):
+        """
+        This method is called from IdP in two different cases:
+        1. We sent a logout request to IdP and IdP responses to this method (LogoutResponse)
+        2. IdP sends logout request to us and we respond to IdP (LogoutRequest handling)
+
+        :param req: request calling this method
+        :param args: args
+        :param kwargs: kwargs
+        :return: redirect
+        """
+        provider = request.env["auth.saml.provider"].sudo().search([], limit=1)
+        client = provider._get_client_for_provider(
+            request.httprequest.url_root.rstrip("/")
+        )
+        # LogoutResponse handling STARTS
+        if "SAMLResponse" in kwargs:
+            _logger.warning("LogoutResponse handling started")
+            try:
+                response = client.parse_logout_request_response(
+                    kwargs.get("SAMLResponse"), BINDING_HTTP_REDIRECT
+                )
+            except StatusError as e:
+                response = None
+                _logger.warning("Error logging out from remote provider: " + str(e))
+
+            if response and response.status_ok():
+                # Successfull LogoutResponse handling
+                return werkzeug.utils.redirect("/", 303)
+        # LogoutResponse handling ENDS
+
+        # LogoutRequest handling STARTS
+        elif "SAMLRequest" in kwargs:
+            # How do we figure out the NameID
+            _logger.warning("LogoutRequest handling started")
+
+            # TODO: This is ugly workaround to get the NameID
+            # most likely the correct procedure is to get the NameID Odoo
+            # details (cache, session, ...). Now we get it from the requests
+            # so it's always true.
+            xmlreq = client.parse_logout_request(
+                xmlstr=kwargs.get("SAMLRequest"),
+                binding=BINDING_HTTP_REDIRECT,
+                relay_state=kwargs.get("RelayState", ""),
+            )
+            name_id = xmlreq.message.name_id
+            saml_token = (
+                request.env["auth_saml.token"]
+                .sudo()
+                .search(
+                    [
+                        ("saml_name_id", "=", code(name_id)),
+                    ]
+                )
+            )
+
+            if not saml_token:
+                # No SAML token, skip
+                _logger.warning("SAML token not found, aborting...")
+                return werkzeug.utils.redirect("/", 303)
+
+            http_info = client.handle_logout_request(
+                request=kwargs.get("SAMLRequest"),
+                name_id=name_id,
+                binding=BINDING_HTTP_REDIRECT,
+                relay_state=kwargs.get("RelayState", ""),
+            )
+            # Destroy user sessions in Odoo
+            target_uid = saml_token.user_id.id
+            sessions = http.root.session_store.list()
+            for sess in sessions:
+                session_store = http.root.session_store
+                session = session_store.get(sess)
+                if session.sid and session.uid == target_uid:
+                    session_store.delete(session)
+
+            return werkzeug.utils.redirect(http_info.get("headers")[0][1], 303)
+        # LogoutRequest handling ENDS
+
+        # If we end up here, then request wasn't LogoutRequest or LogoutResponse
+        # --> Error?
+        _logger.warning("Request wasn't LogoutRequest or LogoutResponse, error?")
+        return werkzeug.utils.redirect("/", 303)
