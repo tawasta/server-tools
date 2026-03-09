022c2a8e3b8234ef953726d75ad7a5348b71b07b
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-09 09:39:06 +0200

    [MIG] 19.0 auth_signup_disable_autoinvite

---
+++ auth_signup_disable_autoinvite/models/res_users.py
@@ -0,0 +1,9 @@
+from odoo import api, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    @api.model
+    def create(self, vals):
+        return super(ResUsers, self.with_context(no_reset_password=True)).create(vals)
