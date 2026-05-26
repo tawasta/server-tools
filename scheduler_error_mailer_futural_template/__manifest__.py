a969b80bdd76bd09833acdaccbfbbae090af9918
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-05-26 13:10:38 +0300

    Add module scheduler_error_mailer_futural_template

---
+++ scheduler_error_mailer_futural_template/__manifest__.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2026- Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Scheduler error mailer - Futural template",
+    "summary": "Adds Futural template to Scheduler error mailer and sets it as default",
+    "version": "19.0.1.0.0",
+    "category": "tools",
+    "website": "https://github.com/tawasta/server-tools",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "post_init_hook": "post_init_hook",
+    "depends": ["scheduler_error_mailer"],
+    "data": ["data/ir_cron_email_tpl.xml"],
+}
